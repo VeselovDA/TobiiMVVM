1f038bdb561c0cf62694ea85cfd7803aa8d9deb5
VeselovDA <dimonwes@gmail.com>
2021-04-07 23:17:20 +0300

    add code for open Window with MVVM

--- TobiiMVVM/App.xaml.cs
+++ TobiiMVVM/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TobiiMVVM.ViewModels;
+using TobiiMVVM.Views;
 
 namespace TobiiMVVM
 {
@@ -13,5 +15,26 @@ namespace TobiiMVVM
     /// </summary>
     public partial class App : Application
     {
+        public DisplayRootRegistry displayRootRegistry = new DisplayRootRegistry();
+        MainWindowVM mainWindowViewModel;
+
+        public App()
+        {
+            displayRootRegistry.RegisterWindowType<MainWindowVM, MainWindow>();
+            // displayRootRegistry.RegisterWindowType<OtherWindowViewModel, ChildWindow>();
+            //displayRootRegistry.RegisterWindowType<DialogWindowViewModel, DialogWindow>();
+            displayRootRegistry.RegisterWindowType<SettingWindowVM, SettingWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            //base.OnStartup(e);
+
+            mainWindowViewModel = new MainWindowVM();
+
+            await displayRootRegistry.ShowModalPresentation(mainWindowViewModel);
+
+            Shutdown();
+        }
     }
 }
