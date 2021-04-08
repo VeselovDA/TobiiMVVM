e2ff114867efc66b91dd79145e41bbc8893126a3
VeselovDA <dimonwes@gmail.com>
2021-04-08 13:33:19 +0300

    add code to mainVM for VideoStream add interface setting add class for VideoStream

---
+++ TobiiMVVM/Models/VideoStream.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace TobiiMVVM.Models
+{
+   
+    class VideoStream: IDisposable
+    {
+        VideoCapture videoCapture;
+        Action<BitmapImage> toSetterCamera;
+        
+        public  VideoStream  (Action<BitmapImage> toSetterCamera,int deviseCameraNum)
+        {
+            this.toSetterCamera = toSetterCamera;
+            videoCapture = new VideoCapture(deviseCameraNum);
+            videoCapture.ImageGrabbed += VideoCapture_ImageGrabbed;
+            videoCapture.Start();
+
+
+
+
+        }
+        private void VideoCapture_ImageGrabbed(object sender, EventArgs e)
+        {
+
+            Mat mat = new Mat();
+            videoCapture.Retrieve(mat);
+            BitmapImage bi = ToBitmapImage(mat.ToImage<Bgr, byte>().ToBitmap());
+            bi.Freeze();
+            toSetterCamera(bi);
+        }
+
+
+
+
+
+
+
+        BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+                return bitmapImage;
+            }
+        }
+
+        public void Dispose()
+        {
+            videoCapture.Stop();
+            videoCapture.Dispose();
+        }
+    }
+}
--- TobiiMVVM/ViewModels/MainWindowVM.cs
+++ TobiiMVVM/ViewModels/MainWindowVM.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 using TobiiMVVM.Models;
 using TobiiMVVM.ViewModels.Base;
 
@@ -12,17 +15,80 @@ namespace TobiiMVVM.ViewModels
 {
     class MainWindowVM : BaseVM
     {
+        #region fields
+        private BitmapImage _Camera1;
+        private BitmapImage _Camera2;
+        private string checkCameras="Загрузка камер";
+        VideoStream videoStream1;
+        VideoStream videoStream2;
+        delegate void AccountHandler(string message);
+        event AccountHandler Notify;
+       
+        #endregion
         public ICommand OpenSetting { get; }
         private bool CanOpenSettingExecute(object p) => true;
         private async void OnOpenSettingExecuted(object p)
         {
+            if(videoStream1!=null)
+                videoStream1.Dispose();
+            if (videoStream2 != null)
+                 videoStream2.Dispose();
             var displayRootRegistry = (Application.Current as App).displayRootRegistry;
             var newWindow = new SettingWindowVM();
             //displayRootRegistry.ShowPresentation(newWindow);
             await displayRootRegistry.ShowModalPresentation(newWindow);
         }
+        public ICommand Load { get; }
+        private bool CanLoadExecute(object p) => true;
+        private void OnLoadExecuted(object p)
+        {
+            Notify += MainWindowVM_Notify;
+            var task=Task.Run(() =>
+            {
+                 videoStream1 = new VideoStream(setCamera1,1);
+                 videoStream2 = new VideoStream(setCamera2,2);
+                Notify("Загрузка завершена");
+
+    });
+            
+            
+        }
+
+        private void MainWindowVM_Notify(string message)
+        {
+            CheckCameras = message;
+        }
+        #region Field for Binding
+        public string CheckCameras
+        {
+            get => checkCameras;
+            set => Set(ref checkCameras, value);
+        }
+        public BitmapImage Camera1
+        {
+            get => _Camera1;
+            set => Set(ref _Camera1, value);
+        }
+
+        public BitmapImage Camera2
+        {
+            get => _Camera2;
+            set => Set(ref _Camera2, value);
+        }
+        //упаковка ссвойства в метод для передачи в класс
+        void setCamera1(BitmapImage im)
+        {
+            Camera1 = im;
+        }
+        void setCamera2(BitmapImage im)
+        {
+            Camera2 = im;
+        }
+        #endregion
+
         public MainWindowVM()
         {
+            Load = new LambdaCommand(OnLoadExecuted, CanLoadExecute);
             OpenSetting = new LambdaCommand(OnOpenSettingExecuted, CanOpenSettingExecute);
         }
     }
